0a503c4984069d13e6ae6b8e49c2a423f6dfb9eb
Shawn <shawn.harrington2776@gmail.com>
2022-01-19 21:41:02 -0800

    finished proj

--- my_proj_fe/src/components/routing/PrivateRoute.js
+++ my_proj_fe/src/components/routing/PrivateRoute.js
@@ -1,21 +1,19 @@
-import { Component } from 'react';
-import { Redirect, Route } from 'react-router-dom';
+
+
+import { Switch, Route } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render = {
-        (props) => {
-          localStorage.getItem("authToken") ? (
-            <Component {...props} />
-          ) : (
-            <Redirect to = "/login" />
-          );
-        }
+      render={(props) => 
+        localStorage.getItem("authToken") ? (
+        <Component {...props} /> ) : (
+          <Switch to="/login" />
+        )
       }
     />
-  )
-}
+  );
+};
 
 export default PrivateRoute
